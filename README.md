520e3f7a943b43be33b771b40190a2c139ddcf6c
hqjma <hqjma@sina.com>
2014-02-24 17:15:59 +0800

    1. add window-numbering.el 2. delete el-get

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
-emacs
-=====
-
 emacs configure file
+---
+以下介绍了目前我的emacs的配置情况及待完善的地方。
 
-1. markdown格式说明  
-要生成支持markdown格式的文本，需先安装markdown。在ubuntu下，执行命令`sudo aptitude install markdown`即可。
+markdown  
+===
+- 安装markdown，在ubuntu下，执行如下命令
+```c
+sudo aptitude install markdown
+```
 
-2. w3m  
-emacs可通过w3m浏览网页。其安装步骤如下  
-    a. 如果想通过cvs安装emacs-w3m，需先安装cvs，安装后执行  
-   `cvs -d :pserver:anonymous@cvs.namazu.org:/storage/cvsroot co w3m`
-       即可。亦可从官网下载emacs-w3m解压安装  
-    b. 确保安装了autoconf。在终端输入`autoconf`，生成configure文件  
-    c. 执行`./configure`  
-    d. 执行`make; sudo make install`  
-    e. 配置emacs.el  
-    f. 打开emcas，输入`M+x w3m`即可
+w3m
+===
+emacs可通过w3m浏览网页，其安装步骤如下：  
+1. 从[w3c Download](http://emacs-w3m.namazu.org/index-en.html#download)下载`emacs-w3m`。
+2. 确保安装了`autoconf`，在`emacs-w3m`的解压目录下执行`autoconf`，生成configure文件。
+3. 执行`./configure; make; sudo make install`安装w3m
+4. 配置emacs.el。
+5. 打开emacs,在`w3m`模式下即可上网。
 
-
+待完成
+===
+- 自动补全，参考[company-mode](https://github.com/company-mode/company-mode)
+- 邮件，(mew)
+- 目录结构，参考![目标图](http://dl2.iteye.com/upload/attachment/0054/7917/7431fe27-9246-3ff0-aff9-99f1a095aa16.png)
+- 代码跳转
+- ...
 
 
 
--- init.el
+++ init.el
@@ -5,24 +5,14 @@
 (require 'w3m)
 (require 'emms)
 
+;; window-numbering
+;; emacs 用 M+num 跳转
+(require 'window-numbering)
 
-
-;(add-to-list 'load-path "~/software/emacs-plugin/w3m/")
-;(require 'w3m)
-;(require 'w3m-load)
-;;(autoload 'w3m "w3m" "interface for w3m on emacs" t)
-;;(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-;;(autoload 'w3m-search "w3m-search" "Search words using emacs-w3m." t)
-;
-;;settings
-;(setq w3m-use-cookies t)
-;(setq w3m-command-arguments '("-cookie" "-F"))
-;(setq w3m-home-page "http://www.google.com")
-;
-;;(require 'mime-w3m) 
-;(setq w3m-default-display-inline-image t) 
-;(setq w3m-default-toggle-inline-images t)
-;
+;;company
+;;(add-to-list 'load-path "~/.emacs.d/company-mode")
+;;(add-hook 'after-init-hook 'global-company-mode)
+;;(require 'company)
 
 ;;支持markdown模式
 (add-to-list 'load-path "~/.emacs.d")
@@ -35,17 +25,16 @@
 (setq default-buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-
 ;;el-get
-(add-to-list 'load-path "~/.emacs.d/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
+;;(add-to-list 'load-path "~/.emacs.d/el-get")
+;;(unless (require 'el-get nil 'noerror)
+;;  (with-current-buffer
+;;      (url-retrieve-synchronously
+;;       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;;    (goto-char (point-max))
+;;    (eval-print-last-sexp)))
+;;(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;;(el-get 'sync)
 
 ;;自动补全--jedi
 ;;(setq jedi:setup-keys t)
